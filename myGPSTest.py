9cb22aedb2971fae4d41227ef623770e8314dec1
RileyMKDixon <rmdixon@ualberta.ca>
2019-03-29 01:13:34 -0600

    Add test file to test gpsClass.py

---
+++ myGPSTest.py
@@ -0,0 +1,26 @@
+from gpsClass import SmartAVLGPS
+
+myGPS = SmartAVLGPS()
+myGPS.start()
+
+while(True):
+	print("---------GPS DATA-----------")
+	dataList = myGPS.get_data()
+	if dataList is not None:
+		current_timestamp = dataList[0]
+		current_longitude = dataList[1]
+		current_latitude = dataList[2]
+		current_speed = dataList[3]
+		print("Timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+			current_timestamp.tm_day,
+			current_timestamp.tm_mon,
+			current_timestamp.tm_year,
+			current_timestamp.tm_hour,
+			current_timestamp.tm_min,
+			current_timestamp.tm_sec))
+	else
+		print("No data to report")
+	
+	time_before_sleep = time.monotonic()
+	while(time.monotonic() - time_before_sleep < 1.0):
+		time.sleep(0.2) #Let the thread block instead of busy wait
